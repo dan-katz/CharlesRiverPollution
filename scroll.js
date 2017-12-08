b7f7e48f9b8605adfafca172c414606cf552f504
danielkatz125 <danielkatz125@gmail.com>
2017-12-07 20:20:12 -0500

    add loadJSON function

--- scroll.js
+++ scroll.js
@@ -50,6 +50,21 @@ function handleContainerExit(response) {
   graphic.classed('is-bottom', response.direction === 'down');
 }
 
+function loadJSON() {
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'sites.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            var data = JSON.parse(xobj.responseText);
+            console.log(data)
+            init();
+          }
+    };
+    xobj.send(null);
+ }
+
 function init() {
   // 1. force a resize on load to ensure proper dimensions are sent to scrollama
   handleResize();
@@ -70,4 +85,4 @@ function init() {
   window.addEventListener('resize', handleResize);
 }
 // kick things off
-init();
+loadJSON();
